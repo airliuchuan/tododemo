a655aaaf406dc16b26472389a2d30b4f01cbb3fa
airliuchuan <wangyiyouxiang23@126.com>
2017-09-28 09:25:09 +0800

    Sf
    Sf

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -207,6 +207,37 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+                test: /\.less$/,
+                use: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader'),
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                                autoprefixer({
+                                    browsers: [
+                                        '>1%',
+                                        'last 4 versions',
+                                        'Firefox ESR',
+                                        'not ie < 9', // React doesn't support IE8 anyway
+                                    ],
+                                    flexbox: 'no-2009',
+                                }),
+                            ],
+                        },
+                    },
+                    {
+                        loader: require.resolve('less-loader'),
+                        options: {
+                            modifyVars: { "@primary-color": "#1DA57A" },
+                        },
+                    },
+                ],
+            },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
